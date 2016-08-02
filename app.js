f6781f39aa6efdefb4fccf333efc0a6fa6d6251a
avalokkumar <avalokkumar@gmail.com>
2016-08-02 13:00:16 +0530

    app.js get modularized into app, routes and server js

--- app.js
+++ app.js
@@ -1,73 +1,37 @@
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+module.exports = function(){
 
-var mongojs = require('mongojs');
-var app = express();
-var bodyParser = require('body-parser');
+    var express = require('express')
+      , routes = require('./routes')
+      , user = require('./routes/user')
+      , path = require('path');
 
-var db = mongojs('contactlist', ['contactlist']);
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
+    var app = express();
+    var bodyParser = require('body-parser');
+    // all environments
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(bodyParser.json());
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    // development only
+    if ('development' == app.get('env')) {
+      app.use(express.errorHandler());
+    }
+
+    app.get('/users', user.list);
+
+    app.get('/contactlist', routes.getContactList);
+
+    app.post('/contactlist', routes.addContact);
+
+    app.delete('/contactlist/:id', routes.deleteContact);
+
+    app.get('/contactlist/:id', routes.getContactById);
+
+    app.put('/contactlist/:id', routes.updateContact);
 }
-
-app.get('/users', user.list);
-
-app.get('/contactlist', function(req, res){
-	console.log('Received a GET request!');
-	db.contactlist.find(function(err, docs){
-		res.json(docs);
-	})
-});
-
-app.post('/contactlist', function(req, res){
-	console.log(req.body);
-	db.contactlist.insert(req.body, function(err, doc) {
-		res.json(doc);
-	});
-});
-
-app.delete('/contactlist/:id', function(req, res){
-	var id = req.params.id;
-	console.log(id);
-	db.contactlist.remove({_id : mongojs.ObjectId(id)}, function(err, doc){
-		res.send(doc);
-	})
-});
-
-app.get('/contactlist/:id', function(req, res){
-	var id = req.params.id;
-	console.log(id);
-	db.contactlist.findOne({_id : mongojs.ObjectId(id)}, function(err, doc){
-		res.json(doc);
-	});
-});
-
-app.put('/contactlist/:id', function(req, res){
-	var id = req.params.id;
-	console.log(id);
-	db.contactlist.findAndModify({query : {_id : mongojs.ObjectId(id)}, 
-		update : {$set: {name : req.body.name, email : req.body.email, number : req.body.number}},
-		new : true}, function(err, doc){
-			res.json(doc);
-	});
-});
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,61 @@
-
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+module.exports = function() {
+    var db = mongojs("contactlist", [ "contactlist" ]), functions = {};
+    functions.index = function(req, res) {
+        res.render("index", {
+            title: "Express"
+        });
+    };
+    functions.getContactList = function(req, res) {
+        console.log("Received a GET request!");
+        db.contactlist.find(function(err, docs) {
+            if (err) {
+                throw err;
+            }
+            res.json(docs);
+        });
+    };
+    functions.addContact = function(req, res) {
+        console.log(req.body);
+        db.contactlist.insert(req.body, function(err, doc) {
+            res.json(doc);
+        });
+    };
+    functions.deleteContact = function(req, res) {
+        var id = req.params.id;
+        console.log(id);
+        db.contactlist.remove({
+            _id: mongojs.ObjectId(id)
+        }, function(err, doc) {
+            res.send(doc);
+        });
+    };
+    functions.getContactById = function(req, res) {
+        var id = req.params.id;
+        console.log(id);
+        db.contactlist.findOne({
+            _id: mongojs.ObjectId(id)
+        }, function(err, doc) {
+            res.json(doc);
+        });
+    };
+    functions.updateContact = function(req, res) {
+        var id = req.params.id;
+        console.log(id);
+        db.contactlist.findAndModify({
+            query: {
+                _id: mongojs.ObjectId(id)
+            },
+            update: {
+                $set: {
+                    name: req.body.name,
+                    email: req.body.email,
+                    number: req.body.number
+                }
+            },
+            new: true
+        }, function(err, doc) {
+            res.json(doc);
+        });
+    };
+    return functions;
 };
